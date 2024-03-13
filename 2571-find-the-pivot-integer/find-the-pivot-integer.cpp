0854ad7c7c497907b796364e652e25ae2e3d3b8c
Rohit Mokashi <97401042+rohitmokashi@users.noreply.github.com>
2024-03-13 11:12:27 +0530

    Time: 70 ms (5.10%) | Memory: 7.3 MB (41.86%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+
+        if (n==1) return 1;
+
+        int i = 1, j = n;
+        int l_sum = i, r_sum = j;
+        i++;j--;
+        
+        cout << i << " " << j << endl;
+
+        while (i <= j) {
+            if (l_sum == r_sum && i==j) return i;
+
+            while (l_sum < r_sum) {
+                l_sum += i;
+                i++;
+                cout << "i++ " << l_sum << endl;
+            }
+            while (r_sum < l_sum) {
+                r_sum += j;
+                j--;
+                cout << "j-- " << r_sum << endl;
+            }
+
+            if (l_sum == r_sum && i<j) r_sum += j--;
+            
+        }
+
+        cout << l_sum << " " << r_sum << endl;
+
+        return -1;
+    }
+};
